11bbcee3ee783160295ca3ee6f7af9080382fcc0
Luiz Zarowny <luizmz96@gmail.com>
2025-01-13 15:52:57 -0300

    finish project

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,10 +1,22 @@
+import { schedulesDay } from "./load"
+import { scheduleCancel } from "../../services/schedule-cancel"
 const periods = document.querySelectorAll(".period")
 
-periods.forEach((period) =>{
-  period.addEventListener("click", (event) =>{
-    if(event.target.classList.contains("cancel-icon")){
-      
+periods.forEach((period) => {
+  period.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
+      const item = event.target.closest("li")
+
+      const { id } = item.dataset
+      if (id) {
+        const isConfirm = confirm(
+          "Tem certeza que seja cancelar esse agendamento?"
+        )
+        if (isConfirm) {
+          await scheduleCancel({ id })
+          schedulesDay()
+        }
+      }
     }
   })
-
 })
---
+++ src/services/schedule-cancel.js
@@ -0,0 +1,13 @@
+import { apiConfig } from "./api-config"
+
+export async function scheduleCancel({ id }) {
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
+      method: "DELETE",
+    })
+    alert("Agendamento cancelado com sucesso!")
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível cancelar. Tente novamente!")
+  }
+}
